2404d706ec2bd2b2956549792e71502912c34514
Eleftherios Avramidis <ea461@cam.ac.uk>
2017-12-22 16:39:08 +0000

    Change so the size of the vector is saved to an int is used as a stopping condition in the for loops.

--- Two_Sum/Two_Sum/Two_Sum.cpp
+++ Two_Sum/Two_Sum/Two_Sum.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <chrono>
 #include <ctime>
+#include <algorithm>
 
 class Solution
 {
@@ -10,14 +11,16 @@ public:
 	{
 		std::vector<int> result;
 
-		for (int i = 0; i < nums.size(); i++)
+		int nums_size = nums.size();
+
+		for (int i = 0; i < nums_size; i++)
 		{
 			if (nums[i] > abs(target))
 			{
 				continue;
 			}
 
-			for (int j = i + 1; j < nums.size(); j++)
+			for (int j = i + 1; j < nums_size; j++)
 			{
 				if (nums[i] + nums[j] == target)
 				{
