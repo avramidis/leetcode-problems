b84f021307066ef90e33fedf10846b91550e23e0
Eleftherios Avramidis <ea461@cam.ac.uk>
2017-12-22 16:04:11 +0000

    Initial commit. Includes a solution for the Two Sum LeetCode problem.

---
+++ Two_Sum/Two_Sum/Two_Sum.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <vector>
+#include <chrono>
+#include <ctime>
+
+class Solution
+{
+public:
+	std::vector<int> twoSum(std::vector<int>& nums, int target)
+	{
+		std::vector<int> result;
+
+		for (int i = 0; i < nums.size(); i++)
+		{
+			if (nums[i] > abs(target))
+			{
+				continue;
+			}
+
+			for (int j = i + 1; j < nums.size(); j++)
+			{
+				if (nums[i] + nums[j] == target)
+				{
+					result.push_back(i);
+					result.push_back(j);
+
+					return result;
+				}
+			}
+		}
+
+		result.push_back(-1);
+		result.push_back(-1);
+		return result;
+	}
+};
+
+int main()
+{
+	Solution solution;
+
+	// Generate input data
+	std::vector<int> input;
+
+	input.push_back(-2);
+	input.push_back(7);
+	input.push_back(-7);
+	input.push_back(-11);
+	input.push_back(15);
+
+	auto start = std::chrono::system_clock::now();
+	std::vector<int> results = solution.twoSum(input, -9);
+	auto end = std::chrono::system_clock::now();
+
+	std::chrono::duration<double> elapsed_seconds = end - start;
+	std::time_t end_time = std::chrono::system_clock::to_time_t(end);
+
+	std::cout << "Elapsed time: " << elapsed_seconds.count() * 1000 << "ms" << std::endl;
+
+	if (results[0] == -1)
+	{
+		std::cout << "No solution was found!" << std::endl;
+	}
+	else
+	{
+		std::cout << "The results is: [" << results[0] << ", " << results[1] << "]" << std::endl;
+	}
+
+	system("pause");
+
+	return 0;
+}
