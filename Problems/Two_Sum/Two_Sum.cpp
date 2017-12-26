a2be37541846ad69a4bf2e8ac34e04d86d70f5ea
Eleftherios Avramidis <el.avramidis@gmail.com>
2017-12-26 20:53:51 +0000

    Removed the main function and added unit test cases.

--- Problems/Two_Sum/Two_Sum.cpp
+++ Problems/Two_Sum/Two_Sum.cpp
@@ -1,8 +1,11 @@
+#define CATCH_CONFIG_MAIN
+
 #include <iostream>
 #include <vector>
 #include <chrono>
 #include <ctime>
 #include <algorithm>
+#include "catch.hpp"
 
 class Solution
 {
@@ -49,43 +52,74 @@ public:
 	}
 };
 
-int main()
+TEST_CASE("Two sum")
 {
 	Solution solution;
-
-	// Generate input data
 	std::vector<int> input;
 
-	input.push_back(-3);
-	input.push_back(4);
-	input.push_back(3);
-	input.push_back(90);
+	SECTION("Input set 1") {
 
-	//for (int i = 0; i < 100; i++)
-	//{
-	//	input.push_back(i);
-	//}
+		input.push_back(-3);
+		input.push_back(4);
+		input.push_back(3);
+		input.push_back(90);
 
-	auto start = std::chrono::system_clock::now();
-	std::vector<int> results = solution.twoSum(input, 0);
-	//std::vector<int> results = solution.twoSum(input, 99 + 98);
-	auto end = std::chrono::system_clock::now();
+		std::vector<int> results = solution.twoSum(input, 0);
 
-	std::chrono::duration<double> elapsed_seconds = end - start;
-	std::time_t end_time = std::chrono::system_clock::to_time_t(end);
-
-	std::cout << "Elapsed time: " << elapsed_seconds.count() * 1000 << "ms" << std::endl;
-
-	if (results[0] == -1)
-	{
-		std::cout << "No solution was found!" << std::endl;
-	}
-	else
-	{
-		std::cout << "The results is: [" << results[0] << ", " << results[1] << "]" << std::endl;
+		REQUIRE((results[0] == 0 && results[1] == 2));
 	}
 
-	system("pause");
+	SECTION("Input set 2") {
 
-	return 0;
+		input.push_back(-3);
+		input.push_back(4);
+		input.push_back(3);
+		input.push_back(90);
+
+		std::vector<int> results = solution.twoSum(input, 93);
+
+		REQUIRE((results[0] == 2 && results[1] == 3));
+	}
+
 }
+
+//int main()
+//{
+//	Solution solution;
+//
+//	// Generate input data
+//	std::vector<int> input;
+//
+//	input.push_back(-3);
+//	input.push_back(4);
+//	input.push_back(3);
+//	input.push_back(90);
+//
+//	//for (int i = 0; i < 100; i++)
+//	//{
+//	//	input.push_back(i);
+//	//}
+//
+//	auto start = std::chrono::system_clock::now();
+//	std::vector<int> results = solution.twoSum(input, 0);
+//	//std::vector<int> results = solution.twoSum(input, 99 + 98);
+//	auto end = std::chrono::system_clock::now();
+//
+//	std::chrono::duration<double> elapsed_seconds = end - start;
+//	std::time_t end_time = std::chrono::system_clock::to_time_t(end);
+//
+//	std::cout << "Elapsed time: " << elapsed_seconds.count() * 1000 << "ms" << std::endl;
+//
+//	if (results[0] == -1)
+//	{
+//		std::cout << "No solution was found!" << std::endl;
+//	}
+//	else
+//	{
+//		std::cout << "The results is: [" << results[0] << ", " << results[1] << "]" << std::endl;
+//	}
+//
+//	system("pause");
+//
+//	return 0;
+//}
