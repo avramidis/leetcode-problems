921e353cddb6c84480c144690848430313d2cac6
Eleftherios Avramidis <el.avramidis@gmail.com>
2017-12-27 09:18:35 +0000

    Deleted commented deprecated code and removed not needed includes.

--- Problems/Two_Sum/Two_Sum.cpp
+++ Problems/Two_Sum/Two_Sum.cpp
@@ -2,9 +2,6 @@
 
 #include <iostream>
 #include <vector>
-#include <chrono>
-#include <ctime>
-#include <algorithm>
 #include "catch.hpp"
 
 class Solution
@@ -24,15 +21,6 @@ public:
 					continue;
 				}
 			}
-
-			//if (target != 0)
-			//{
-			//	if (nums[i] > abs(target))
-			//	{
-			//		std::cout << "In the if-continue branch" << std::endl;
-			//		continue;
-			//	}
-			//}
 
 			for (int j = i + 1; j < nums_size; j++)
 			{
@@ -92,44 +80,3 @@ TEST_CASE("Two sum")
 	}
 
 }
-
-//int main()
-//{
-//	Solution solution;
-//
-//	// Generate input data
-//	std::vector<int> input;
-//
-//	input.push_back(-3);
-//	input.push_back(4);
-//	input.push_back(3);
-//	input.push_back(90);
-//
-//	//for (int i = 0; i < 100; i++)
-//	//{
-//	//	input.push_back(i);
-//	//}
-//
-//	auto start = std::chrono::system_clock::now();
-//	std::vector<int> results = solution.twoSum(input, 0);
-//	//std::vector<int> results = solution.twoSum(input, 99 + 98);
-//	auto end = std::chrono::system_clock::now();
-//
-//	std::chrono::duration<double> elapsed_seconds = end - start;
-//	std::time_t end_time = std::chrono::system_clock::to_time_t(end);
-//
-//	std::cout << "Elapsed time: " << elapsed_seconds.count() * 1000 << "ms" << std::endl;
-//
-//	if (results[0] == -1)
-//	{
-//		std::cout << "No solution was found!" << std::endl;
-//	}
-//	else
-//	{
-//		std::cout << "The results is: [" << results[0] << ", " << results[1] << "]" << std::endl;
-//	}
-//
-//	system("pause");
-//
-//	return 0;
-//}
