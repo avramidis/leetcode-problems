2b2be235cb56c0cb28f5fbf72b0db0e53e7790c1
Eleftherios Avramidis <el.avramidis@gmail.com>
2017-12-29 15:35:54 +0000

    Small optimisation changes.

--- Problems/LongestSubstringWithoutRepeatingCharacters/LongestSubstringWithoutRepeatingCharacters.cpp
+++ Problems/LongestSubstringWithoutRepeatingCharacters/LongestSubstringWithoutRepeatingCharacters.cpp
@@ -5,9 +5,9 @@
 
 class Solution {
 public:
-	bool uniquestring(std::string s) {
-		for (size_t i = 0; i < s.size(); i++) {
-			for (size_t j = i + 1; j < s.size(); j++) {
+	bool uniquestring(std::string s, int start, int end) {
+		for (size_t i = start; i < end; i++) {
+			for (size_t j = i + 1; j <= end; j++) {
 				if (s[i] == s[j]) {
 					return false;
 				}
@@ -28,16 +28,12 @@ public:
 		}
 
 		int result = 1;
-		
+
 		for (size_t i = 0; i < s.size(); i++) {
-			for (size_t j = i+1; j < s.size(); j++) {
-				std::string temp_s = s.substr(i, j - i + 1);
-				if (uniquestring(temp_s)) {
-					if (temp_s.size() > result) {
-						result = temp_s.size();
-						if (result > s.size() - i) {
-							return result;
-						}
+			for (size_t j = i + result; j < s.size(); j++) {
+				if (uniquestring(s, i, j)) {
+					if (j - i + 1 > result) {
+						result = j - i + 1;
 					}
 				}
 			}
@@ -56,7 +52,7 @@ TEST_CASE("Unique characters in string")
 		bool result = false;
 		BENCHMARK("Check if the string is made of unique characters")
 		{
-			result = solution.uniquestring(input);
+			result = solution.uniquestring(input, 0, 7);
 		}
 		REQUIRE(result == false);
 	}
@@ -66,23 +62,23 @@ TEST_CASE("Unique characters in string")
 		bool result = false;
 		BENCHMARK("Check if the string is made of unique characters")
 		{
-			result = solution.uniquestring(input);
+			result = solution.uniquestring(input, 0, 0);
 		}
 		REQUIRE(result == true);
 	}
 
-	SECTION("Input set 2") {
+	SECTION("Input set 3") {
 		std::string input = "au";
 		bool result = false;
 		BENCHMARK("Check if the string is made of unique characters")
 		{
-			result = solution.uniquestring(input);
+			result = solution.uniquestring(input, 0, 1);
 		}
 		REQUIRE(result == true);
 	}
 }
 
-TEST_CASE("Two sum")
+TEST_CASE("Longest Substring Without Repeating Characters")
 {
 	Solution solution;
 
@@ -144,5 +140,15 @@ TEST_CASE("Two sum")
 			result = solution.lengthOfLongestSubstring(input);
 		}
 		REQUIRE(result == 0);
+	}
+
+	SECTION("Input set 6") {
+		std::string input = "dvdf";
+		int result = 0;
+		BENCHMARK("Calculate length of the longest substring with unique characters")
+		{
+			result = solution.lengthOfLongestSubstring(input);
+		}
+		REQUIRE(result == 3);
 	}
 }
